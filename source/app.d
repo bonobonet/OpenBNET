23fc936b7c0f0749138c9fb832173bd882516faf
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-03-20 12:50:46 +0200

    - Setup router and start listening - Added a basic `/` route handler

--- source/app.d
+++ source/app.d
@@ -2,6 +2,11 @@ import std.stdio;
 
 import vibe.d;
 
+void homeHandler(HTTPServerRequest req, HTTPServerResponse resp)
+{
+	resp.writeBody("Hello");
+}
+
 void main()
 {
 	writeln("Edit source/app.d to start your project.");
@@ -9,6 +14,12 @@ void main()
 	HTTPServerSettings httpSettings = new HTTPServerSettings();
 	httpSettings.bindAddress = ["::"];
 	httpSettings.bindPort = 8002;
+
+	URLRouter router = new URLRouter();
+
+	router.get("/", &homeHandler);
+
+	listenHTTP(router, httpSettings);
 
 	runApplication();
 }
