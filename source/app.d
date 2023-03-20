96dd801a83eac96001a4a356297e334a97c72c84
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-03-20 12:48:38 +0200

    - Added vibe as a dependency - Added some settings

--- source/app.d
+++ source/app.d
@@ -1,6 +1,14 @@
 import std.stdio;
 
+import vibe.d;
+
 void main()
 {
 	writeln("Edit source/app.d to start your project.");
+
+	HTTPServerSettings httpSettings = new HTTPServerSettings();
+	httpSettings.bindAddress = ["::"];
+	httpSettings.bindPort = 8002;
+
+	runApplication();
 }
