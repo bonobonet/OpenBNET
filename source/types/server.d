26ef461b65497bda2b6c8069cfb1c9b260a251e0
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-03-21 23:59:19 +0200

    - Parse the JSON from `server.list` JSON RPC call

--- source/types/server.d
+++ source/types/server.d
@@ -25,16 +25,31 @@ unittest
     }
 }
 
+import std.datetime.date;
+import std.string;
+
 public class Server
 {
     private string name;
     private string sid;
+    private string hostname; // TODO: MAke an address object
+    private string details;
+
+
+    /* information about this server (link-wise) */
+    private string infoString;
+    private string uplinkServer;
+    private long num_users;
+    private DateTime boot_time;
+    private bool synced, ulined;
+
+    private string software, protocol;
+
 
     private this()
     {
 
     }
-
 
     public static Server fromJSON(JSONValue jsonIn)
     {
@@ -42,7 +57,29 @@ public class Server
 
         server.name = jsonIn["name"].str();
         server.sid = jsonIn["id"].str();
+        server.hostname = jsonIn["hostname"].str();
+        server.details = jsonIn["details"].str();
 
+        /* Extract all information from the `server {}` block */
+        JSONValue serverBlock = jsonIn["server"];
+        server.infoString = serverBlock["info"].str();
+        server.uplinkServer = serverBlock["uplink"].str();
+        server.num_users = serverBlock["num_users"].integer();
+
+        // Strip off the .XXXZ
+        string bootTime = serverBlock["boot_time"].str();
+        long dotPos = indexOf(bootTime, ".");
+        bootTime = bootTime[0..dotPos];
+        server.boot_time = DateTime.fromISOExtString(bootTime);
+
+        server.synced = serverBlock["synced"].boolean();
+        server.ulined = serverBlock["ulined"].boolean();
+
+        /* Extract all information from the `feature {}` block within the `server {}` block */
+        JSONValue featureBlock = serverBlock["features"];
+        server.software = featureBlock["software"].str();
+        server.protocol = featureBlock["protocol"].str();
+        
 
 
         return server;
