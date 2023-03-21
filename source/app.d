2f0d42d73300faad60768b424b40433292b95b07
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-03-21 12:44:03 +0200

    Channels page
    - Make channel names clickcable such that you can view in-depth information on a given channel
    
    App
    
    - Disabled curl import
    - Added a handler for `/channelinfo?name=<name>`
    - Added an error handler

--- source/app.d
+++ source/app.d
@@ -5,7 +5,7 @@ import vibe.d;
 // TODO: Make configurable via environment variab;e
 string rpcEndpoint = "https://apiuser:password@127.0.0.1:8001/api";
 
-import std.net.curl;
+// import std.net.curl;
 
 public class Network
 {
@@ -27,11 +27,37 @@ void channelListHandler(HTTPServerRequest req, HTTPServerResponse resp)
 	resp.render!("channels.dt", network, channels);
 }
 
+void channelInfoHandler(HTTPServerRequest req, HTTPServerResponse resp)
+{
+	/* Extract the parameters */
+	auto params = req.params;
+
+	/* Extract name parameter */
+	if(params.get("name") !is null)
+	{
+		string channelName = params["name"];
+	}
+	/* If not found, throw an error */
+	else
+	{
+		throw new HTTPStatusException(HTTPStatus.badRequest, "Missing channel name parameter");
+	}
+
+	// TODO: Ensure we have a "name" parameter, if not throw an HTTP error
+}
+
 void homeHandler(HTTPServerRequest req, HTTPServerResponse resp)
 {
 	
 
 	resp.writeBody("Hello");
+}
+
+void errorHandler(HTTPServerRequest req, HTTPServerResponse resp, HTTPServerErrorInfo error)
+{
+	// TODO: FInish error page
+	auto request = req;
+	resp.render!("error.dt", error, request);
 }
 
 void main()
@@ -46,10 +72,14 @@ void main()
 	httpSettings.bindAddresses = ["::"];
 	httpSettings.port = 8002;
 
+	import std.functional : toDelegate;
+	httpSettings.errorPageHandler = toDelegate(&errorHandler);
+
 	URLRouter router = new URLRouter();
 
 	router.get("/", &homeHandler);
 	router.get("/channels", &channelListHandler);
+	router.get("/channelinfo", &channelInfoHandler);
 
 	// Setup serving of static files
 	router.get("/assets/table.css", serveStaticFile("assets/table.css"));
