6e268843a7cc09756764d3f1662a7db896a08f18
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-04-02 17:34:35 +0200

    Stats
    - Debug print the incoming JSON

--- source/openbnet/types/stats.d
+++ source/openbnet/types/stats.d
@@ -21,6 +21,9 @@ public class Stats
     {
         Stats stats = new Stats();
 
+        import std.stdio;
+        writeln(jsonIn);
+
         JSONValue serverBlock = jsonIn["server"];
         stats.serverCount = serverBlock["total"].integer();
 
